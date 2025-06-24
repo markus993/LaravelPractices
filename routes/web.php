057a3ab5b86d28b519a9887f825d53afa02b2471
Marco Mayor <markus993@hotmail.com>
2025-06-24 18:11:52 -0500

    Add Basic Controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('HolaMundo');
 });
+
 Route::get('/holaArray', function () {
     $frutas = ['manzana', 'banana', 'naranja'];
     return view('holaArray.index', compact('frutas'));
 });
+
+//testController
+Route::get('/testController', [UserController::class, 'index']);
